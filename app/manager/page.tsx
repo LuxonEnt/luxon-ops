f2b7f6b289badf10288fcf5a3da94e71762f3fb3
LuxonEnt <brayan@go2freedom.org>
2026-05-09 17:39:46 -0700

    Create ManagerPage component for manager portal

---
+++ app/manager/page.tsx
@@ -0,0 +1,8 @@
+export default function ManagerPage() {
+  return (
+    <main className="min-h-screen bg-black p-8 text-white">
+      <h1 className="text-4xl font-bold">Manager Portal</h1>
+      <p className="mt-3 text-zinc-400">Manager page is working.</p>
+    </main>
+  );
+}
