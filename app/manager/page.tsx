8966d5b290bd87d059fa6cd8d7eaa5110601ed7c
LuxonEnt <brayan@go2freedom.org>
2026-06-30 16:54:51 -0700

    Add lunch break email sending functionality

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -2454,6 +2454,7 @@ export default function ManagerPage() {
                           onDeleteAssignment={deleteAssignment}
                           onCancelAndNotifyAssignment={cancelAndNotifyAssignment}
                           onSendAssignmentReminder={sendAssignmentReminder}
+                          onSendLunchBreakEmail={sendManualLunchBreakEmail}
                           onSaveReview={saveManagerReview}
                           onApproveHours={approveHours}
                           onUpdateAssignment={updateAssignment}
